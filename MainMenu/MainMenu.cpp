7ab6cefe5e3c4f80f7121e0a271213cf2dab1649
Jonathan Narine <jonathan.narine@ontariotechu.net>
2019-10-18 23:20:54 -0400

    //UpdateMainMenu&UploadMancalaGame

--- MainMenu/MainMenu.cpp
+++ MainMenu/MainMenu.cpp
@@ -1,11 +1,26 @@
+#include "MainMenu.h"
+#include <iostream>
+
+using namespace std;
+
+/*
+Mancala
+  __  __                                  _
+ |  \/  |                                | |
+ | \  / |   __ _   _ __     ___    __ _  | |   __ _
+ | |\/| |  / _` | | '_ \   / __|  / _` | | |  / _` |
+ | |  | | | (_| | | | | | | (__  | (_| | | | | (_| |
+ |_|  |_|  \__,_| |_| |_|  \___|  \__,_| |_|  \__,_|
+
+*/
+
+void createTitle() {
 
 
 
+}
 int main() {
-
-
-
-
-
+	void Instructions();
 	return 0;
 }
+
